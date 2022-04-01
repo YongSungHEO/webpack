46f3be8df4b2bcc377b8b7017acccaa61f9da224
yongsungHeo <ysheo94@gmail.com>
2022-04-02 00:40:48 +0900

    css-loader, style-loader, Asset Modules samples are added.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+import './app.css';
+import small from './assets/img/small.png';
+
 document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML = `
+        <img src="${small}" />
+    `;
     console.log('DOMContentLoaded');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,28 @@ module.exports = {
         path: path.resolve('./dist')
     },
     module: {
-        rules: [{
-            test: /\.js$/,  // .js 확장자로 끝나는 모든 파일
-            use: [path.resolve('./src/loadertest')]
-        }]
+        rules: [
+            {
+                test: /\.js$/,  // .js 확장자로 끝나는 모든 파일
+                use: [path.resolve('./src/loadertest')],
+                exclude: [/node_modules/],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],    // 배열의 뒤에서부터 읽는다.
+            },
+            {
+                test: /\.png$/,
+                type: 'asset',  // Default로 크기가 8kb 미만인 파일은 inline 모듈로 처리, 그렇지 않으면 resource 모듈로 처리
+                generator: {
+                    filename: 'static/[name][hash][ext][query]'
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 5 * 1024 // 5kb
+                    }
+                }
+            },
+        ],
     }
 }
